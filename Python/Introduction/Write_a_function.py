8fa27e72a454b49946e42b8c36a3f82c43f89055
Nathan <nathanlawira@gmail.com>
2024-06-22 11:40:10 +0700

    Added Write a function solution

---
+++ Python/Introduction/Write_a_function.py
@@ -0,0 +1,12 @@
+def is_leap(year):
+    if int(year)%4==0:
+        if int(year)%100==0:
+            if int(year)%400==0:
+                leap=True
+            else:
+                leap=False
+        else:
+            leap=True
+    else:
+        leap=False
+    return leap
