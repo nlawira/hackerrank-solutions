5dd1ec4fe8c4624a535781fbee244ce414bf877a
Nathan <nathanlawira@gmail.com>
2024-07-12 22:13:23 +0800

    Added solution for Find Angle MBC

---
+++ Python/Math/Find Angle MBC.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+import math
+
+AB = int(input())
+BC = int(input())
+print(str(round(math.degrees(math.atan(AB/BC))))+f'{chr(176)}')
