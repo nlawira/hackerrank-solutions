03012b78f10ed3eff33878b5d822cad2aaa51404
Nathan <nathanlawira@gmail.com>
2024-08-04 18:54:04 +0800

    Added a solution for Array Mathematics

---
+++ Python/Numpy/Array Mathematics.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+N, M = map(int, input().split())
+
+A=np.array([input().split() for _ in range(N)],int)
+B=np.array([input().split() for _ in range(N)],int)
+
+print(A+B)
+print(A-B)
+print(A*B)
+print(A//B)
+print(A%B)
+print(A**B)
