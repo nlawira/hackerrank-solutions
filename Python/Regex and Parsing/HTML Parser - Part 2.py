6ab7dad48535550636e61110ca289102fe23af43
Nathan <nathanlawira@gmail.com>
2024-08-12 00:47:29 +0800

    Added a solution for HTML Parser - Part 2

---
+++ Python/Regex and Parsing/HTML Parser - Part 2.py
@@ -0,0 +1,20 @@
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    def handle_comment(self, data):
+        if "\n" in data:
+            print(">>> Multi-line Comment\n"+data)
+        else:
+            print(">>> Single-line Comment\n"+data)
+    def handle_data(self, data):
+        if data != "\n":
+            print(">>> Data\n"+data)
+          
+html = ""       
+for i in range(int(input())):
+    html += input().rstrip()
+    html += '\n'
+    
+parser = MyHTMLParser()
+parser.feed(html)
+parser.close()
